baeafb867ee60c07f198273bb14dc1817ad41a6d
vlaaaaaad <vladzapryagaylo@gmail.com>
2024-10-08 17:51:50 +0300

    added table with basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { ordersData } from "./ordersData.ts";
 
 function App() {
+  const [products, setProducts] = useState(
+    ordersData.flatMap((order) => order.products)
+  );
+  const [available, setAvailable] = useState(true);
+  const [pending, setPending] = useState(true);
+
+  useEffect(() => {
+    const reducedProducts = products.reduce((accumulator, currentValue) => {
+      const existingProduct = accumulator.find(
+        (product) =>
+          product.name === currentValue.name &&
+          product.status.value === currentValue.status.value
+      );
+      if (existingProduct) {
+        existingProduct.qty += currentValue.qty;
+      } else {
+        accumulator.push({ ...currentValue });
+      }
+      return accumulator;
+    }, []);
+    setProducts(reducedProducts);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="filterSection">
+          <div className="checkbox">
+            <input
+              type="checkbox"
+              id="avaliable"
+              checked={available}
+              disabled={pending}
+              onChange={() => {
+                setAvailable(!available);
+              }}
+            />
+            <label htmlFor="avaliable">Avaliable</label>
+          </div>
+          <div className="checkbox">
+            <input
+              type="checkbox"
+              id="pending"
+              checked={pending}
+              disabled={available}
+              onChange={() => {
+                setPending(!pending);
+              }}
+            />
+            <label htmlFor="pending">Pending</label>
+          </div>
+          <button
+            disabled={!available && !pending}
+            onClick={() => {
+              setAvailable(false);
+              setPending(false);
+            }}
+          >
+            Clear filters
+          </button>
+        </div>
+        <table>
+          {/* <caption>Orders</caption> */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Location</th>
+              <th>UOM</th>
+              <th>Qty</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products
+              .filter(
+                (product) =>
+                  (product.status.value === "Available" && available) ||
+                  (product.status.value === "Pending" && pending)
+              )
+              .map((product) => (
+                <tr key={crypto.randomUUID()}>
+                  <td key={crypto.randomUUID()}>{product.name}</td>
+                  <td key={crypto.randomUUID()}>{product.location.name}</td>
+                  <td key={crypto.randomUUID()}>{product.uom.value}</td>
+                  <td key={crypto.randomUUID()}>{product.qty}</td>
+                  <td key={crypto.randomUUID()}>{product.status.value}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
---
+++ src/ordersData.ts
@@ -0,0 +1,282 @@
+export const ordersData = [
+  {
+    id: 1,
+    products: [
+      {
+        id: 1,
+        name: "Product A",
+        location: {
+          id: 1,
+          name: "Main Warehouse",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 10,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+      {
+        id: 2,
+        name: "Product B",
+        location: {
+          id: 2,
+          name: "East Distribution Center",
+        },
+        uom: {
+          id: 2,
+          value: "Tons",
+        },
+        qty: 5,
+        status: {
+          id: 2,
+          value: "Pending",
+        },
+      },
+      {
+        id: 3,
+        name: "Product C",
+        location: {
+          id: 3,
+          name: "West Storage",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 15,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+    ],
+  },
+  {
+    id: 2,
+    products: [
+      {
+        id: 1,
+        name: "Product A",
+        location: {
+          id: 1,
+          name: "Main Warehouse",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 7,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+      {
+        id: 4,
+        name: "Product F",
+        location: {
+          id: 4,
+          name: "South Depot",
+        },
+        uom: {
+          id: 2,
+          value: "Tons",
+        },
+        qty: 25,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+      {
+        id: 2,
+        name: "Product B",
+        location: {
+          id: 2,
+          name: "East Distribution Center",
+        },
+        uom: {
+          id: 2,
+          value: "Tons",
+        },
+        qty: 10,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+    ],
+  },
+  {
+    id: 3,
+    products: [
+      {
+        id: 3,
+        name: "Product C",
+        location: {
+          id: 3,
+          name: "West Storage",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 20,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+      {
+        id: 5,
+        name: "Product D",
+        location: {
+          id: 5,
+          name: "North Hub",
+        },
+        uom: {
+          id: 2,
+          value: "Tons",
+        },
+        qty: 12,
+        status: {
+          id: 2,
+          value: "Pending",
+        },
+      },
+      {
+        id: 1,
+        name: "Product A",
+        location: {
+          id: 1,
+          name: "Main Warehouse",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 8,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+    ],
+  },
+  {
+    id: 4,
+    products: [
+      {
+        id: 2,
+        name: "Product B",
+        location: {
+          id: 2,
+          name: "East Distribution Center",
+        },
+        uom: {
+          id: 2,
+          value: "Tons",
+        },
+        qty: 7,
+        status: {
+          id: 2,
+          value: "Pending",
+        },
+      },
+      {
+        id: 5,
+        name: "Product D",
+        location: {
+          id: 5,
+          name: "North Hub",
+        },
+        uom: {
+          id: 2,
+          value: "Tones",
+        },
+        qty: 18,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+      {
+        id: 6,
+        name: "Product E",
+        location: {
+          id: 6,
+          name: "Central Warehouse",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 5,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+    ],
+  },
+  {
+    id: 5,
+    products: [
+      {
+        id: 4,
+        name: "Product F",
+        location: {
+          id: 4,
+          name: "South Depot",
+        },
+        uom: {
+          id: 2,
+          value: "Tons",
+        },
+        qty: 20,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+      {
+        id: 3,
+        name: "Product C",
+        location: {
+          id: 3,
+          name: "West Storage",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 10,
+        status: {
+          id: 2,
+          value: "Pending",
+        },
+      },
+      {
+        id: 6,
+        name: "Product E",
+        location: {
+          id: 6,
+          name: "Central Warehouse",
+        },
+        uom: {
+          id: 1,
+          value: "Each",
+        },
+        qty: 7,
+        status: {
+          id: 1,
+          value: "Available",
+        },
+      },
+    ],
+  },
+];
