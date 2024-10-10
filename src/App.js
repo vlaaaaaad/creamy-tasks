19d33198cbbde858c7f8b1b129414ab867604906
vlaaaaaad <vladzapryagaylo@gmail.com>
2024-10-10 22:38:03 +0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import "./App.css";
 import { ProductTable } from "./products/components/ProductTable";
 
 const App = () => {
   return (
     <div className="App">
-      <ProductTable />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductTable />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
